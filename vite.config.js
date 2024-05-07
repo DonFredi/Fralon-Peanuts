f19756dabe31068313d95676f3cd24b0483e83ae
DonFredi <dondadi.fn@gmail.com>
2024-05-07 19:47:26 +0300

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-const base = process.env.NODE_ENV === 'production' ? '/fralon-peanuts/' : '/';
 
 export default defineConfig({
-  base: base,
+  base: "/fralon-peanuts/",
   plugins: [react()],
   build: {
     outDir: 'build', // Specify the output directory as 'build'
