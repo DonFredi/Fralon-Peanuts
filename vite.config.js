58d642b846b368e91fa8a186c14de6267fdac417
DonFredi <dondadi.fn@gmail.com>
2024-05-08 10:29:49 +0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: "/fralon-peanuts/",
   plugins: [react()],
   build: {
     outDir: 'build', // Specify the output directory as 'build'
