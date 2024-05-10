5a4d32590693d6367e5f0231e92b5af7671f1a1f
DonFredi <dondadi.fn@gmail.com>
2024-05-10 16:53:29 +0300

    kl

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
+        <div className='p-4'>
             <h1>404 - Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
+            <Link to="/">Visit homepage</Link>
         </div>
     );
 };
