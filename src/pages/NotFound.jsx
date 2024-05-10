3ea1769b0d94d2703564b72ccd2ef6e5e4133ccd
DonFredi <dondadi.fn@gmail.com>
2024-05-10 16:56:39 +0300

    kl

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,7 +5,9 @@ const NotFound = () => {
     return (
         <div className='p-4'>
             <h1>404 - Not Found</h1>
-            <Link to="/">Visit homepage</Link>
+            <Link
+                className='text-blue-400 text-underline'
+                to="/">Visit homepage</Link>
         </div>
     );
 };
